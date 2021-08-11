c9ef5e467ceff2c8b651bb3674daf9293ba73ed7
kuriyantoadi <melatikuning212@gmail.com>
2021-08-11 22:35:23 +0700

    11 agustus 2021, update 2

--- application/views/form_upload.php
+++ application/views/form_upload.php
@@ -1,9 +1,11 @@
 <html>
-    <head>
-        <title> Belajar Upload di Codeigniter - Warung Belajar -</title>
-    </head>
-    <body>
-        <?php 
+
+<head>
+  <title> Belajar Upload di Codeigniter - Warung Belajar -</title>
+</head>
+
+<body>
+  <?php
         if(isset($error))
         {
             echo "ERROR UPLOAD : <br/>";
@@ -11,28 +13,29 @@
             echo "<hr/>";
         }
         ?>
-        <form method="post" enctype="multipart/form-data" action="<?php echo base_url(); ?>index.php/upload/proses">
-            <div>Berkas   1: </div>
-        	<div><input type="file" name="berkas[]"></div>
-            <div>Keterangan 1 : </div>
-            <div><textarea name="keterangan_berkas[]"></textarea></div>
-            <hr/>
-            <div>Berkas   2: </div>
-        	<div><input type="file" name="berkas[]"></div>
-            <div>Keterangan 2 : </div>
-            <div><textarea name="keterangan_berkas[]"></textarea></div>
-            <hr/>
-            <div>Berkas   3: </div>
-        	<div><input type="file" name="berkas[]"></div>
-            <div>Keterangan 3 : </div>
-            <div><textarea name="keterangan_berkas[]"></textarea></div>
-            <hr/>
-            <div>Berkas   4: </div>
-        	<div><input type="file" name="berkas[]"></div>
-            <div>Keterangan 4 : </div>
-            <div><textarea name="keterangan_berkas[]"></textarea></div>
-            <br/>
-            <div><input type="submit" value="Simpan"/></div>
-        </form>
-    </body>
+  <form method="post" enctype="multipart/form-data" action="<?php echo base_url(); ?>index.php/upload/proses">
+    <div>Berkas 1: </div>
+    <div><input type="file" name="berkas[]"></div>
+    <div>Keterangan 1 : </div>
+    <div><textarea name="keterangan_berkas[]"></textarea></div>
+    <hr />
+    <div>Berkas 2: </div>
+    <div><input type="file" name="berkas[]"></div>
+    <div>Keterangan 2 : </div>
+    <div><textarea name="keterangan_berkas[]"></textarea></div>
+    <hr />
+    <div>Berkas 3: </div>
+    <div><input type="file" name="berkas[]"></div>
+    <div>Keterangan 3 : </div>
+    <div><textarea name="keterangan_berkas[]"></textarea></div>
+    <hr />
+    <div>Berkas 4: </div>
+    <div><input type="file" name="berkas[]"></div>
+    <div>Keterangan 4 : </div>
+    <div><textarea name="keterangan_berkas[]"></textarea></div>
+    <br />
+    <div><input type="submit" value="Simpan" /></div>
+  </form>
+</body>
+
 </html>
